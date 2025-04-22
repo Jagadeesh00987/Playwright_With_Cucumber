dcfae79de569af5d2c2f72b1dd946b8d9f69ec50
jagadeesh <jagadeesh@cloudrevelinnovation.com>
2025-04-22 17:33:17 +0530

    ngork added and webhook implemented

--- src/tests/steps/SearchFun.js
+++ src/tests/steps/SearchFun.js
@@ -1,20 +1,21 @@
-const{Given,And,Then,When}=require('@cucumber/cucumber')
-const{expect}=require('@playwright/test')
-const SearchproductsHome=require('C:/Users/jagadeesh/Desktop/PLaywrightPractice/src/Pages/SearchFun.js')
+const { Given, And, Then, When } = require('@cucumber/cucumber')
+const { expect } = require('@playwright/test')
+const SearchproductsHome = require('C:/Users/jagadeesh/Desktop/PLaywrightPractice/src/Pages/SearchFun.js')
 
-
-
-When('User Select certain category to search for the product', async function () {
-    this.search= new SearchproductsHome(this.page);
-    await this.search.clickDropdown('Women');
-    console.log("Clicked")
+When(
+  'User Select certain category to search for the product',
+  async function () {
+    this.search = new SearchproductsHome(this.page)
+    await this.search.clickDropdown('Women')
+    console.log('Clicked')
     //await this.search.allOptionsAndClick('Traditional Wear');
-    });
+  }
+)
 
-  When('User enter a search keyword to find a product', async function () {
-    await this.search.searchproducts('cloths')
-  });
+When('User enter a search keyword to find a product', async function () {
+  await this.search.searchproducts('cloths')
+})
 
-  When('User clicks the search button', async function () {
-    console.log("Product Searched Successful")
-  });
+When('User clicks the search button', async function () {
+  console.log('Product Searched Successful')
+})
